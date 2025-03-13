9754e3232d6b770fadb70fc7229923907e7e8dca
Kh11811 <khalilgabssi8@gmail.com>
2025-03-13 12:29:15 +0100

    Create tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include "main.cpp"
+
+TEST(UserServiceTest, RegisterUserTest) {
+    UserService userService;
+    userService.RegisterUser("testuser", "Test@1234");
+    EXPECT_TRUE(userService.LoginUser("testuser", "Test@1234"));
+}
+
+TEST(UserServiceTest, LoginUserTest) {
+    UserService userService;
+    userService.RegisterUser("testuser", "Test@1234");
+    EXPECT_FALSE(userService.LoginUser("testuser", "wrongpassword"));
+}
+
+TEST(PasswordValidatorTest, ValidatePasswordTest) {
+    PasswordValidator validator;
+    EXPECT_TRUE(validator.validate("Test@1234"));
+    EXPECT_FALSE(validator.validate("weak"));
+}
