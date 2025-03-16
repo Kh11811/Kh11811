3a1d7bc4dd9e63750725b9644a3c5e2900488a24
Kh11811 <khalilgabssi8@gmail.com>
2025-03-16 23:13:09 +0100

    Update projet2.cpp

--- projet2.cpp
+++ projet2.cpp
@@ -251,13 +251,20 @@ int main(){
         size_t comma1=line.find(',');
         size_t comma2=line.find(',',comma1+1);
         size_t comma3=line.find(',',comma2+1);
+        size_t end=line.find(' ',comma3);
+        cout<<end<<endl;
         string username=line.substr(0,comma1);
         string mode=line.substr(comma1+1,comma2-comma1-1);
         string pass=line.substr(comma2+1,comma3-comma2-1);
-        string salt=line.substr(comma3+1);
+        string salt=line.substr(comma3+1,end);
+        cout<<salt<<endl;
         cout<<username<<'\t'<<mode<<'\t'<<pass<<'\t'<<salt<<endl;
         test_function(mode,username,pass,salt,testcases);
     }
+    SaltGenerator salt1(5);
+    string pass="Khis onit@@";
+    string salt=salt1.generateSalt();
     
+    Hasher hash(pass,salt);
     return 0;
 }
